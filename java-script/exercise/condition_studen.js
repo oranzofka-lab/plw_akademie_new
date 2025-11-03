64a691321b1eef41c02f2c25ff7c88fe1d195430
oranzofka <oranzofka@gmail.com>
2025-11-03 16:53:11 +0100

    neco napisu

---
+++ java-script/exercise/condition_studen.js
@@ -0,0 +1,9 @@
+const pocetZiskanychBodu = 75;
+const pocetBodu = 75;
+const jmenoStudenta = "Luca Kolar";
+
+if (pocetZiskanychBodu >= pocetBodu) {
+  console.log(`${jmenoStudenta} Uspel.`);
+} else if (pocetZiskanychBodu < 75) {
+  console.log(`${jmenoStudenta} Neuspel`);
+}
