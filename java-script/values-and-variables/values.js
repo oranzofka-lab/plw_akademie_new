623da57a876e924488ad30203d8a2a1e281546a0
oranzofka <oranzofka@gmail.com>
2025-11-03 15:46:22 +0100

    kalkulacka lepe

--- java-script/values-and-variables/values.js
+++ java-script/values-and-variables/values.js
@@ -1,6 +1,4 @@
-funkcion calculator ()
-var result (15 + 5);
-var result (474 - 520);
-var result (5822 * 55); // Nejde mi napsat sestka, promin
-var result (532 / 5);
-console.log result;
+console.log(15 + 5);
+console.log(474 - 520);
+console.log(5822 * 55); // Nejde mi napsat sestka, promin
+console.log(532 / 5);
